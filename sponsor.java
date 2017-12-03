d0df5b62eee0646d8f5a87f973179e78e006cd40
TinaBryan <tdmak63@gmail.com>
2017-12-03 14:59:12 -0500

    added sponsor li to separate file

---
+++ sponsor.java
@@ -0,0 +1,6 @@
+String[] sponsor = {"Golf Sponsors: ", "Price Webber", "WLKY 32", "Golf Digest", "Golf Magazine",
+										"Dicks Sporting Goods", "Louisville Athletic Club (LAC)",
+										"U of L Golf Club", "Cherry Valley Golf Course", "Valley View Golf Club",
+										"Old Capital Golf Club", "Chariot Run", "Country Oaks Golf Club",
+										"French Lick Resort Dye Course", "French Lick Resort Donald Ross Course",
+										"Christmas Lake Golf Course"};
